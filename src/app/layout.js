818c111dd90a6a3df3ed8f7f918b14bbd1f53ede
kuldeeplohar <loharkuldeep169@gmail.com>
2023-10-02 11:57:57 +0530

    c3

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,7 +7,9 @@ import "../../public/assets/vendor/remixicon/remixicon.css"
 import "../../public/assets/vendor/simple-datatables/style.css"
 import "../../public/assets/css/style.css"
 import './globals.css'
+import Script from 'next/script'
 import { Inter } from 'next/font/google'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,6 +21,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        
+          <Script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></Script>
+          <Script src="assets/vendor/apexcharts/apexcharts.min.js"></Script>
+          <Script src="assets/vendor/chart.js/chart.umd.js"></Script>
+          <Script src="assets/vendor/echarts/echarts.min.js"></Script>
+          <Script src="assets/vendor/quill/quill.min.js"></Script>
+          <Script src="assets/vendor/simple-datatables/simple-datatables.js"></Script>
+          <Script src="assets/vendor/tinymce/tinymce.min.js"></Script>
+          <Script src="assets/vendor/php-email-form/validate.js"></Script>
+
+        
+          <Script src="assets/js/main.js"></Script>
+      </body>
     </html>
   )}
